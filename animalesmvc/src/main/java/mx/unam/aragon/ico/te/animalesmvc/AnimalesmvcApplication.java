3f9a7fa6716e74c3559ca6af96821c1676d12bbd
AndreaAvi <andreaca@outlook.com>
2025-06-03 00:52:23 -0600

    Primer commit: Creación del proyecto

---
+++ animalesmvc/src/main/java/mx/unam/aragon/ico/te/animalesmvc/AnimalesmvcApplication.java
@@ -0,0 +1,13 @@
+package mx.unam.aragon.ico.te.animalesmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalesmvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimalesmvcApplication.class, args);
+	}
+
+}
