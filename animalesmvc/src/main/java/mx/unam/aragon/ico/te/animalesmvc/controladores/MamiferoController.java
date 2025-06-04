2113b17f37bd26f568611246a3cf9f171994131a
AndreaAvi <andreaca@outlook.com>
2025-06-03 19:07:17 -0600

    Mamíferos completos

--- animalesmvc/src/main/java/mx/unam/aragon/ico/te/animalesmvc/controladores/MamiferoController.java
+++ animalesmvc/src/main/java/mx/unam/aragon/ico/te/animalesmvc/controladores/MamiferoController.java
@@ -44,6 +44,15 @@ public class MamiferoController {
     // READ del CRUD
     @GetMapping("/lista")
     public String listaMamiferos(Model model) {
+        // Simulación previa:
+        /*
+        List<Mamifero> mamiferos = List.of(
+                new Mamifero(1, "Jaguar (Panthera onca)", 1.8, "Selva tropical", "Carnívoro", "América Central y Sur", 12, "Vulnerable", "https://es.wikipedia.org/wiki/Jaguar", "https://files.worldwildlife.org/wwfcmsprod/images/Jaguar_GPN7474/story_full_width/4q517ahb0m_Responsible_Forestry_8.10.2012_Biodiversiry_Loss_HI_7474.jpg"),
+                new Mamifero(2, "Lobo gris (Canis lupus)", 1.2, "Bosques y tundras", "Carnívoro", "América del Norte y Eurasia", 13, "Preocupación menor", "https://es.wikipedia.org/wiki/Lobo", "https://www.telemundo.com/sites/nbcutelemundo/files/images/promo/article/2017/05/02/lobo-gris-mexicano.jpg"),
+                new Mamifero(3, "Elefante africano (Loxodonta)", 3.3, "Sabana", "Herbívoro", "África", 60, "Vulnerable", "https://es.wikipedia.org/wiki/Elefante_africano", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcST6BnN2Nvej56plqy27ajZnOSMeNEkUJgjNw&s"),
+                new Mamifero(4, "Elefante africano (Loxodonta)", 3.3, "Sabana", "Herbívoro", "África", 60, "Vulnerable", "https://es.wikipedia.org/wiki/Elefante_africano", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcST6BnN2Nvej56plqy27ajZnOSMeNEkUJgjNw&s")
+        );*/
+
         try {
             List<Mamifero> mamiferos = mamiferoService.obtenerTodos();
             model.addAttribute("mamiferos", mamiferos);
@@ -71,6 +80,20 @@ public class MamiferoController {
     // UPDATE del CRUD
     @GetMapping("/modificar/{id}")
     public String mostrarFormularioModificacion(@PathVariable int id, Model model) {
+        // Simulación previa
+        /*
+        Mamifero mamifero = new Mamifero(
+                id,
+                "Tigre de Bengala",
+                3.1,
+                "Selvas tropicales y manglares",
+                "Carnívoro",
+                "Asia (principalmente India y Bangladés)",
+                15,
+                "En peligro",
+                "https://es.wikipedia.org/wiki/Tigre_de_Bengala",
+                "https://upload.wikimedia.org/wikipedia/commons/5/56/Tigre_de_Bengala.jpg"
+        );*/
         try {
             Mamifero mamifero = mamiferoService.buscarPorId(id);
             model.addAttribute("mamifero", mamifero);
